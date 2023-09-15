47399dd11fa9293b5c4c608a74fd00cf18eb4b9f
arakhorluk <jan.horak@tutanota.com>
2023-09-15 09:58:31 +0200

    addition of accepting cmd args and reading file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::{env, fs};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let file_path = &args[1];
+    let search_query = &args[2];
+
+    println!("arg 1: {}", file_path);
+    println!("arg 2: {}", search_query);
+
+    let file_content = fs::read_to_string(file_path).expect("couldn't read file");
+
+    let result = file_content.contains(search_query);
+
+    if result {
+        println!("file contains the word: \"{}\"", search_query);
+    }
 }
